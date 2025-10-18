37b34127dc2d8520b71c562fa081293d7009a6a2
= <=>
2025-10-18 10:19:25 -0300

    Banco

---
+++ banco.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+conexao =mysql.connector.connect(
+    host="mysql.railway.internal",
+    user="root",
+    password="niZZKBtVchrdhWTgjhoTVILgFVFMTWce",
+    database="railway",
+    port=3306
+)
